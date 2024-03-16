268791ce2265a78b49d9b5b33ef30f8cc66cf3db
caoshenghui <csh0518@live.com>
2024-03-16 21:58:58 +0800

    update wt

--- windows-terminal/install.ps1
+++ windows-terminal/install.ps1
@@ -2,9 +2,13 @@ if (-not (Get-Command wt -ErrorAction SilentlyContinue)) {
     Exit
 }
 
-$cpath = Split-Path $MyInvocation.MyCommand.Path
+$script_path = Split-Path $MyInvocation.MyCommand.Path
 $file_name = "settings.json"
-$settings_path = (Get-Item "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState\")
-$settings_file = Join-Path $settings_path $file_name
+$wt_path = (Get-Item "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_*")
+$settings_file = Join-Path $wt_path "LocalState" $file_name
 Remove-Item $settings_file 2>$null
-New-Item -ItemType HardLink -Path $settings_file -Target (Join-Path $cpath $file_name) *>$null
+# New-Item -ItemType HardLink -Path $settings_file -Target (Join-Path $script_path $file_name) *>$null
+Copy-Item -Path (Join-Path $script_path $file_name) -Destination $settings_file  *>$null
+
+# wt icon
+Copy-Item -Path (Join-Path $script_path "assets") -Destination (Join-Path $wt_path "RoamingState") -Recurse *>$null
